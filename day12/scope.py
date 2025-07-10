21a6fa47cdfb35cb97348bc5e22a72e5565e8fb6
Hanna <hanna.hematii@gmail.com>
2025-07-10 09:28:36 +0330

    meaning of the scope, nothing more

---
+++ day12/scope.py
@@ -0,0 +1,13 @@
+boom = 1
+
+def isboom():
+    boom = 2
+    print(f"this is inside the jar{boom}")
+
+isboom()
+print(f"this is outside the jar{boom}")
+
+# scope is like a box that your stuff is in there.
+# when you put more stuff inside a small box,
+# only the ppl who can open the box can use the stuff.
+# if sb is out of the box they cant use the stuff in there.
