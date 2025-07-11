e404b04c5d85400f26f8cbbbf0971dc4afeb7ae3
Hanna <hanna.hematii@gmail.com>
2025-07-11 10:54:47 +0330

    day 12 completed, it was all my own code.

---
+++ day12/guess_the_number.py
@@ -0,0 +1,52 @@
+import random
+
+# print("""
+#
+#  ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░░▒▓███████▓▒░▒▓███████▓▒░      ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░      ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓██████████████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓███████▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░                ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░
+# ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░                ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░
+# ░▒▓█▓▒▒▓███▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░  ░▒▓██████▓▒░░▒▓██████▓▒░          ░▒▓█▓▒░   ░▒▓████████▓▒░▒▓██████▓▒░        ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓███████▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░
+# ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░
+#  ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓████████▓▒░▒▓███████▓▒░▒▓███████▓▒░          ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░
+#
+#
+# """)
+
+youready = input("Hello welcome to game! we are going to do it in a persian way! ready?")
+
+if youready == "yes":
+    print("I'm thinking  of a number between 1 and 100")
+    num = random.randrange(100)
+    def_or_easy = input("do you want to play 'easy' ya 'hard'? type it down")
+    user_num = []
+
+
+    def users_number():
+        guess = int(input("make your guess:"))
+        user_num.append(guess)
+        print(f"your guesses are {user_num}")
+        if num < guess:
+            print("too high")
+        elif num > guess:
+            print("too low")
+        elif num == guess:
+            print(f"you got it Dokhtar/Pesar e GOL. the answer was {guess}")
+
+
+    if def_or_easy == "hard":
+        print("You only allow 5 attempts, ok? choose wisely")
+        for hard in range(5):
+            users_number()
+            if hard == 4:
+                print("you are out of guess my dear")
+                print(f"the answer was {num}")
+    elif def_or_easy == "easy":
+        print("You only allow 10 attempts, ok? choose wisely")
+        for hard in range(10):
+            users_number()
+            if hard == 9:
+                print("you are out of guess my dear")
+                print(f"the answer was {num}")
+else:
+    print("man dige nemidonam")
--- day12/scope.py
+++ day12/scope.py
@@ -1,6 +1,8 @@
 boom = 1
 
 def isboom():
+    global boom
+    #kolan intorie ke baes mishe on var ke biron az in box tarahi shode ghabele estefade beshe.
     boom = 2
     print(f"this is inside the jar{boom}")
 
@@ -11,3 +13,11 @@ print(f"this is outside the jar{boom}")
 # when you put more stuff inside a small box,
 # only the ppl who can open the box can use the stuff.
 # if sb is out of the box they cant use the stuff in there.
+
+# Global constant: is the cvar that you don't wanna change it, at all
+# like the pi number
+# Global Constant are normally declared in ALL CAPS with a _ in between
+
+PI = 3.14159
+GOOGLE_URL = "https://www.google.com"
+
