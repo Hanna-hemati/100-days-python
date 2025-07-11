cca7eaf30b92ed10912f596be963e5de3e11d49f
Hanna <hanna.hematii@gmail.com>
2025-07-11 11:03:45 +0330

    day 13 debugging and explingn added

---
+++ day13/debugging.py
@@ -0,0 +1,7 @@
+def my_f():
+    for i in range(1, 10):
+        if i == 10:
+            print("you got it")
+
+
+# this code dosen working becouse the for loop can't reach the 10. it's gonna end in 9
